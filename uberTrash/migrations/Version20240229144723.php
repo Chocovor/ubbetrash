47d2351d2930da6297af4ee31fc25e37b986b7ee
[Matias] <monteil.mathias@gmail.com>
2024-02-29 22:59:23 +0100

    first commit

---
+++ uberTrash/migrations/Version20240229144723.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240229144723 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, roles_id INT NOT NULL, INDEX IDX_57698A6A38C751C4 (roles_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE role ADD CONSTRAINT FK_57698A6A38C751C4 FOREIGN KEY (roles_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE role DROP FOREIGN KEY FK_57698A6A38C751C4');
+        $this->addSql('DROP TABLE role');
+    }
+}
